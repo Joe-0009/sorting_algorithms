961b071e52b95f464058a14037ace3e7167226a6
Joe-0009 <youssefrachidi1994@gmail.com>
2023-12-18 09:05:41 -0800

    first task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -36,8 +36,11 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[k] > array[k + 1])
 			{
-				swap_int(array[k], array[k + 1]);
+				temp = array[k];
+				array[k] = array[k +1];
+				array[k + 1] = temp;
 				print_array(array, size);
+
 			}
 		}
 		i++;
