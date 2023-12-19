79279dc3ca98cfb4de227ae06b1c05e2ed2caa0f
Joe-0009 <youssefrachidi1994@gmail.com>
2023-12-19 15:48:17 -0800

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,29 +10,24 @@
  */
 
 
-
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
 	int *min;
+	size_t i, j;
 
-	if (!array || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min = array + i;
 		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < *min)
-				min = array + j;
+			min = (array[j] < *min) ? (array + j) : min;
 
-		}
 		if ((array + i) != min)
 		{
 			swap_int(array + i, min);
 			print_array(array, size);
 		}
 	}
-
 }
